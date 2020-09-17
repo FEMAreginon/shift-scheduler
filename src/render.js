aa7a01f93d042b496b418776412863ec3ccd0fd8
wajeht <zombyard@gmail.com>
2020-09-17 07:29:07 -0500

    First setup

---
+++ src/render.js
@@ -0,0 +1,8 @@
+// const myNotification = new Notification("Title", {
+//     body: "Lorem Ipsum Dolor Sit Amet",
+// });
+
+// myNotification.onclick = () => {
+//     console.log("Notification clicked");
+// };
+
