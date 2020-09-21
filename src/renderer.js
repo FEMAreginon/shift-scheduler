dc491c3ab7399c071f7bab349b42ba7eceec48d9
wajeht <zombyard@gmail.com>
2020-09-20 20:43:59 -0500

    Added clearTabl() and changeDate() function

--- src/renderer.js
+++ src/renderer.js
@@ -5,3 +5,27 @@ function resultHTML() {
 function indexHTML() {
     window.location.href = "index.html";
 }
+
+function addServer() {
+    const serverTextArea = document.getElementById("txt_server").value;
+    const valueOfServerTextArea = document.createTextNode(serverTextArea);
+
+    const serverTableBody = document.getElementById("server-table-body");
+    const newRow = serverTableBody.insertRow(-1);
+    const newCell = newRow.insertCell(0);
+    newCell.appendChild(valueOfServerTextArea);
+}
+
+function clearTable() {
+    const serverTableBody = document.getElementById("server-table-body");
+    while (serverTableBody.rows.length > 0) {
+        serverTableBody.deleteRow(0);
+    }
+
+}
+
+function changeDate() {
+    const todaysDate = new Date();
+    document.getElementById("week-date").innerHTML =
+        "Changed to Week of " + todaysDate.toLocaleDateString().toString();
+}
