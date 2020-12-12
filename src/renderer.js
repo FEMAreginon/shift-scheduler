387f64a5293433039ed48415cb4beb582ad1e2d4
wajeht <zombyard@gmail.com>
2020-12-11 22:05:05 -0600

    Delete uncessary lines

--- src/renderer.js
+++ src/renderer.js
@@ -1,14 +1,6 @@
-function toResultHTML() {
-    window.location.href = "result.html";
-}
-
-function toIndexHTML() {
-    window.location.href = "index.html";
-}
-
 function randomShift() {
-    const SHIFTS = "w ";
-    let resutl = "";
+    const SHIFTS = 'w ';
+    let resutl = '';
 
     for (let i = 0; i < 1; i++) {
         resutl += SHIFTS.charAt(Math.floor(Math.random() * SHIFTS.length));
@@ -18,32 +10,32 @@ function randomShift() {
 }
 
 function randomizeShifts() {
-    const serverTableBody = document.getElementById("server-table-body");
-    const tbody = document.getElementsByTagName("tbody")[0];
-    const tds = tbody.getElementsByTagName("td");
+    const serverTableBody = document.getElementById('server-table-body');
+    const tbody = document.getElementsByTagName('tbody')[0];
+    const tds = tbody.getElementsByTagName('td');
 
     if (serverTableBody.rows.length === 0) {
-        M.toast({ html: "Nothing to randomize!" });
+        M.toast({ html: 'Nothing to randomize!' });
         overlayOn();
     } else {
         // iterate throught all the td tag, then access to the tags that has td center class and change its value.
         for (let i = 0; i < tds.length; i++) {
-            if (tds[i].className == "td-centered") {
+            if (tds[i].className == 'td-centered') {
                 // Set a new width
                 tds[i].innerHTML =
                     "<div contenteditable='true' id='editable'>" +
                     randomShift() +
-                    " / " +
+                    ' / ' +
                     randomShift() +
-                    "</div>";
+                    '</div>';
             }
 
             // some how the codes below doens't work. gonan leave this just to come
             // back later
-            const editableText = document.getElementById("editable");
-            editableText.addEventListener("keydown", function (event) {
-                if (event.key === "Enter" || event.key === "Esc") {
-                    alert("hi");
+            const editableText = document.getElementById('editable');
+            editableText.addEventListener('keydown', function (event) {
+                if (event.key === 'Enter' || event.key === 'Esc') {
+                    alert('hi');
                 }
             });
         }
@@ -54,20 +46,18 @@ function addServer(form) {
     const serverTextArea = document.forms.form_server.txt_server.value;
     const valueOfServerTextArea = document.createTextNode(serverTextArea);
 
-    if (serverTextArea === "" || valueOfServerTextArea == "") {
-        M.toast({ html: "Input a value!" });
+    if (serverTextArea === '' || valueOfServerTextArea == '') {
+        M.toast({ html: 'Input a value!' });
         overlayOn();
         return false;
     } else {
         // add user input to table to display
-        const serverTableBody = document.getElementById("server-table-body");
+        const serverTableBody = document.getElementById('server-table-body');
         const newRow = serverTableBody.insertRow();
         const newCell = newRow.insertCell();
 
         newCell.innerHTML =
-            "<div contenteditable='true' id='editable'><b>" +
-            serverTextArea +
-            "<b></div>";
+            "<div contenteditable='true' id='editable'><b>" + serverTextArea + '<b></div>';
         for (let i = 0; i < 6; i++) {
             newRow.innerHTML += "<td class='td-centered'>n/a</td>";
         }
@@ -78,17 +68,17 @@ function addServer(form) {
 
 function clearServerTextAreaAfterHittingAddOrEnter() {
     setTimeout(function () {
-        document.getElementById("txt_server").value = "";
+        document.getElementById('txt_server').value = '';
     }, 50);
 }
 
 function clearTable() {
-    const serverTableBody = document.getElementById("server-table-body");
+    const serverTableBody = document.getElementById('server-table-body');
     M.Toast.dismissAll();
 
     // if there is nothing else to clear, display a message with overlay efferct on
     if (serverTableBody.rows.length === 0) {
-        M.toast({ html: "Nothing to clear!" });
+        M.toast({ html: 'Nothing to clear!' });
         overlayOn();
     }
     // otherwise if there are rows, delete the rows when the button is pres
@@ -107,21 +97,21 @@ function getMonday(date) {
 }
 
 // when page loaded display next week monday date
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
     // will get monday date of this week amd add 7 days to return date of next week monday
     let thisWeekMonday = getMonday(new Date());
     thisWeekMonday.setDate(thisWeekMonday.getDate() + 7);
 
-    document.getElementById("week-date-text").innerHTML =
+    document.getElementById('week-date-text').innerHTML =
         "<div contenteditable='true'>" +
-        "Week of " +
+        'Week of ' +
         thisWeekMonday.toLocaleDateString().toString() +
-        "</div>";
+        '</div>';
 });
 
 function printNow() {
-    var divContents = document.getElementById("pdf-content").innerHTML;
-    var a = window.open("", "", "height=1000, width=800");
+    var divContents = document.getElementById('pdf-content').innerHTML;
+    var a = window.open('', '', 'height=1000, width=800');
 
     a.document.write(divContents);
     a.document.close();
@@ -130,10 +120,10 @@ function printNow() {
 
 // this will dim the webapge when user try to add without providing any text aka empty
 function overlayOn() {
-    document.getElementById("overlay").style.display = "block";
+    document.getElementById('overlay').style.display = 'block';
 }
 
 function overlayOff() {
-    document.getElementById("overlay").style.display = "none";
+    document.getElementById('overlay').style.display = 'none';
     M.Toast.dismissAll();
 }
